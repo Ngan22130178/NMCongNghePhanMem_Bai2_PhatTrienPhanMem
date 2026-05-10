0f7f6b07667433953145e05124c62d79d3c8968c
Ngan22130178 <146084366+Ngan22130178@users.noreply.github.com>
2026-05-10 15:20:01 +0700

    Add files via upload
    create project

---
+++ src/main/java/vn/edu/nlu/fit/nmcnpm/my_silly_bestie/MySillyBestieApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.nlu.fit.nmcnpm.my_silly_bestie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySillyBestieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySillyBestieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/edu/nlu/fit/nmcnpm/my_silly_bestie/MySillyBestieApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.nlu.fit.nmcnpm.my_silly_bestie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySillyBestieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
